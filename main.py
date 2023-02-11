b1958d3f920922467ff238b264504abb6be219fc
Iliya Valaei <iliavalaee@gmail.com>
2023-02-11 19:59:30 +0100

    login page is complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-
+import tkinter.messagebox
+pageStatus = 1
 root = Tk()
 
 # general att
@@ -8,18 +9,64 @@ root.geometry("1200x800")
 root.state('zoomed')
 
 
+def pageDecider(value):
+    if value == 1:
+        loginPage()
+    elif value == 2:
+        firstMenu()
+
+
 def loginPage():
+
+    def login():
+        username = entUsername.get()
+        password = entPass.get()
+        try:
+            credential = open(
+                f"C:/Users/GECKO/git-projects/Library management/{username}.txt", "r")
+            if credential.read() != password:
+                tkinter.messagebox.showwarning(
+                    title="error", message="credential is wrong")
+            else:
+                global pageStatus
+                pageStatus = 2
+        except:
+            tkinter.messagebox.showwarning(
+                title="error", message="credential is wrong")
+
     root.config(bg="#559364")
-    canvas = Canvas(root, width=500, height=700,
+    canvas = Canvas(root, width=500, height=500,
                     bg="#AEC09A", highlightthickness=0, highlightbackground="#559364")
     canvas.place(relx=.5, rely=.5, anchor=CENTER)
 
-    entryFont = ("Helvetica Rounded", 16)
+    loginLabel = Label(canvas, text="Login", font=(
+        "Helvetica Rounded", 30), bg="#AEC09A")
+    loginLabel.place(relx=.5, rely=.15, anchor=CENTER)
+
+    lbluser = Label(canvas, text="username", font=(
+        "Helvetica Oblique", 11), bg="#AEC09A")
+    lbluser.place(relx=.262, rely=.323, anchor=CENTER)
+
+    lblpass = Label(canvas, text="password", font=(
+        "Helvetica Oblique", 11), bg="#AEC09A")
+    lblpass.place(relx=.262, rely=.445, anchor=CENTER)
+
+    entryFont = ("Helvetica Rounded", 18)
     entUsername = Entry(canvas, font=entryFont, bg="#e7f0ef", fg="#559364")
-    entUsername.place(relx=.5, rely=.3, anchor=CENTER)
+    entUsername.place(relx=.5, rely=.38, anchor=CENTER)
+    entPass = Entry(canvas, font=entryFont, bg="#e7f0ef", fg="#559364")
+    entPass.place(relx=.5, rely=.5, anchor=CENTER)
+
+    btnLogin = Button(canvas, bg='#559364', text="Login",
+                      font=entryFont, command=login)
+    btnLogin.place(relx=.5, rely=.7, anchor=CENTER)
 
 
-loginPage()
+def firstMenu():
+    btnLogin = Button(root, bg='#559364', text="Login",
+                      )
+    btnLogin.place(relx=.5, rely=.7, anchor=CENTER)
 
 
+pageDecider(pageStatus)
 root.mainloop()
