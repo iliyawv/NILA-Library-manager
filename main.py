337ed56c0f2498110bab7f89f102166a493e33ee
Iliya Valaei <iliavalaee@gmail.com>
2023-02-11 18:31:56 +0100

    first page ui is coming together

--- main.py
+++ main.py
@@ -8,4 +8,18 @@ root.geometry("1200x800")
 root.state('zoomed')
 
 
+def loginPage():
+    root.config(bg="#559364")
+    canvas = Canvas(root, width=500, height=700,
+                    bg="#AEC09A", highlightthickness=0, highlightbackground="#559364")
+    canvas.place(relx=.5, rely=.5, anchor=CENTER)
+
+    entryFont = ("Helvetica Rounded", 16)
+    entUsername = Entry(canvas, font=entryFont, bg="#e7f0ef", fg="#559364")
+    entUsername.place(relx=.5, rely=.3, anchor=CENTER)
+
+
+loginPage()
+
+
 root.mainloop()
